c401b08a0a98bb2ec29357a7ebc4f97aeed56d78
anmol9999 <anmol9chaudhari@gmail.com>
2021-10-06 13:35:09 +0530

    list

---
+++ list/creation.py
@@ -0,0 +1,12 @@
+num = {1,2,3,3,4,7}
+words = {'hi','hola','hello','namaste'}
+mixed = { 'potato',10,'tomato','zomato'}
+tf_list = [True,True,False,True]
+
+print(num)
+print(words)
+print(mixed)
+print(tf_list)
+
+
+marks = [[ 'ravi',30,20,15],[]]
