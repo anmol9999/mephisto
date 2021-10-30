f932580a1ebdf4ba5757b3461d073ba6d9e4d782
anmol9999 <anmol9chaudhari@gmail.com>
2021-10-30 20:27:33 +0530

    libraries

--- classes_objects/operator_overloading.py
+++ classes_objects/operator_overloading.py
@@ -1,34 +1,34 @@
-from _typeshed import Self
+class Shape:
 
-
-class shape:
-
-    def __init__(self,w,h):
-        self.width = w
-        self.height = h
+    def __init__(self,w=0,h=0):
+        self.width=w
+        self.height=h
 
     def area(self):
-        return self.height * self.width
+        return self.height*self.width
 
-    def perimeter(self):
-        return 2 * self.width * self.height
-
-    def __it__(self,other):
+    def parameter(self):
+        return 2*self.height*self.width
+    
+    def __lt__(self,other):
         if self.area() < other.area():
             return True
         return False
 
-    def __repr__(self):
+    def __repr__(self):   # it shows raw representation of a variable
         return str(self.area())
 
-
-items = [shape(2,2), shape(1,5), shape(3,3), shape(10,10), shape(3,2)]
+items=[Shape(2,2),Shape(1,5),Shape(3,3),Shape(10,10),Shape(3,2)]
 print(items)
 
-if shape(3,3) > shape(3,2):
-    print('yes')
+
+if Shape(38,39) > Shape(13,72):
+    print('Yes')
 
 items.sort()
 print(items)
+
+
+
 
         
